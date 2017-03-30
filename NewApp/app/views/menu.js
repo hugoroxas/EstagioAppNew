b06163a7060b018b3340f2ace24377bbbde9805b
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-03-30 15:21:33 +0100

    Mete lá isso

--- NewApp/app/views/menu.js
+++ NewApp/app/views/menu.js
@@ -44,4 +44,10 @@ exports.principal = function(args) {
     stackLayout.addChild(btn5);
     page.content = stackLayout;
 
+    // ORA MUITO BOA TARDE
+    // ESTOU AQUI A PERDER O MEU TEMPO 
+    // A FAZER ESTES COMENTÁRIOS
+    // MAS NÃO FAZ MAL PORQUE
+    // NÃO TENHO MAIS NADA PARA FAZER
+
 }
