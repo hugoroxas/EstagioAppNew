2d8bac3366267ec49a4991b726d5bb9be3a5f58a
hugoroxas <hugo_roxas@live.com.pt>
2017-04-03 12:28:30 +0100

    Grid Working

--- NewApp/platforms/android/src/main/assets/app/views/login-view/login.js
+++ NewApp/platforms/android/src/main/assets/app/views/login-view/login.js
@@ -1,27 +1,46 @@
 var http = require("http");
 var labelModule = require("ui/label");
-var layout = require("ui/layouts/stack-layout");
+var buttonModule = require("ui/button");
+var layout = require("ui/layouts/grid-layout");
 var localStorage = require("nativescript-localstorage");
 var labelNumber = 0;
+var buttonNumber = 0;
 var page;
 var labelArray = Array();
+var buttonArray = Array();
 
 exports.load = function(args) {
 
-    page = args.object;
-
     http.getString("https://luisfranciscocode.000webhostapp.com/webservice.php?format=json&&form=form_login")
     .then(function (r) {
-        
-        var newStackLayout = new layout.StackLayout();
-        console.log(localStorage.getItem("isItLoaded"));
-        var letsSee = localStorage.getItem("isItLoaded");
 
-        if( letsSee === false){
+        page = args.object;
+        
+        var newGridLayout = new layout.GridLayout();
 
         var myJSON = JSON.parse(r);
         var fieldsSize = myJSON.length;
-        console.log(fieldsSize);
+
+        var arrayColumn = Array(fieldsSize);
+        var arrayRow = Array(fieldsSize);
+
+        for ( i = 0 ; i < 3 ; i++ )
+        {
+
+            arrayColumn[i] = new layout.ItemSpec(1, layout.GridUnitType.star);
+            newGridLayout.addColumn(arrayColumn[i]);
+
+        }
+
+         for ( i = 0 ; i < 4 ; i++ )
+        {
+
+            arrayRow[i] = new layout.ItemSpec(1, layout.GridUnitType.star);
+            newGridLayout.addRow(arrayRow[i]);
+
+        }
+
+        console.log(arrayColumn.length);
 
         for ( i = 0 ; i < fieldsSize ; i++ )
         {
@@ -32,8 +51,20 @@ exports.load = function(args) {
                 case "label": labelArray[labelNumber] = new labelModule.Label();
                               labelArray[labelNumber].id = myJSON[i].Fields.id;
                               labelArray[labelNumber].text = myJSON[i].Fields.text;
-                              newStackLayout.addChild(labelArray[labelNumber]);
+                              layout.GridLayout.setColumn(labelArray[labelNumber], parseInt(myJSON[i].Fields.width));
+                              layout.GridLayout.setRow(labelArray[labelNumber], parseInt(myJSON[i].Fields.height));
+                              newGridLayout.addChild(labelArray[labelNumber]);
                               labelNumber++;
+                              
+                break;
+
+                case "button": buttonArray[buttonNumber] = new buttonModule.Button();
+                               buttonArray[buttonNumber].id = myJSON[i].Fields.id;
+                               buttonArray[buttonNumber].text = myJSON[i].Fields.text;
+                               layout.GridLayout.setColumn(buttonArray[buttonNumber], parseInt(myJSON[i].Fields.width));
+                               layout.GridLayout.setRow(buttonArray[buttonNumber], parseInt(myJSON[i].Fields.height));
+                               newGridLayout.addChild(buttonArray[buttonNumber]);
+                               buttonNumber++;
                 break;
 
                 default:
@@ -42,22 +73,11 @@ exports.load = function(args) {
             }
 
         }
-    
-        newStackLayout.backgroundImage = "~/views/login-view/Imagens/souka.jpg";
-        newStackLayout.style.backgroundSize = "cover";
-        newStackLayout.style.backgroundRepeat= "no-repeat";
-        localStorage.setItem("isItLoaded" , true);
-        localStorage.setItem("loadedLayout" , newStackLayout);
-        console("ADDED!!!");
-        page.content = newStackLayout;
-
-    } else {
-
-        console.log("ITS LOADED!!");
-        newStackLayout = localStorage.getItem("loadedLayout")
-        page.content = newStackLayout;
-
-    }
+      
+        newGridLayout.backgroundImage = "~/views/login-view/Imagens/bg2.jpg";
+        newGridLayout.style.backgroundSize = "cover";
+        newGridLayout.style.backgroundRepeat= "no-repeat";
+        page.content = newGridLayout;
 
 }, function (e) {
     alert(e);
