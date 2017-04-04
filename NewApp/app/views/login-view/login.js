9a9b952f9b1417c80cd23fa122ebc574fdb2883a
hugoroxas <hugo_roxas@live.com.pt>
2017-04-04 16:17:09 +0100

    Login Done

--- NewApp/app/views/login-view/login.js
+++ NewApp/app/views/login-view/login.js
@@ -1,36 +1,137 @@
 var http = require("http");
+var toast = require("nativescript-toast");
 var labelModule = require("ui/label");
 var buttonModule = require("ui/button");
 var textFieldModule = require("ui/text-field");
 var imageModule = require("ui/image");
 var layout = require("ui/layouts/grid-layout");
-var layout2 = require("ui/layouts/stack-layout");
 var localStorage = require("nativescript-localstorage");
 var application = require("application");
     application.cssFile = "login.css";
-var labelNumber = 0;
-var buttonNumber = 0;
-var textFieldNumber = 0;
-var imageNumber = 0;
 var page;
-var labelArray = Array();
-var buttonArray = Array();
-var textFieldArray = Array();
-var imageArray = Array();
+
+// --------------------------------------------- //
 
 exports.load = function(args) {
 
-    var k = "indeed";
-    localStorage.setItem( "appVersion" , k );
+    // Vai confirmar a versao
+    page = args.object;
 
-    console.log(localStorage.key(0));
+    http.getString("https://luisfranciscocode.000webhostapp.com/verVersao.php?form=form_login")
+    .then(function (r) {
 
-    if( k == localStorage.getItem( "appVersion" ) ){
+        if( localStorage.getItem( "form_login_appVersion" ) == r ){
 
+            console.log("foi buscar no localstorage");
+
+            // Same version
+
+            var fieldsSize = localStorage.getItem("form_login_typeObject").length;
+            var fieldsArray = new Array();
+            var newGridLayout = new layout.GridLayout();
+
+            var arrayColumn = Array(fieldsSize);
+            var arrayRow = Array(fieldsSize);
+
+            // Create Columns ( a primeira e a ultima sao autosize ,
+            // a do meio ( "1" ) é a que tem um tamanho especifico "pixel" )
+
+            for ( i = 0 ; i < 3 ; i++ )
+            {
+                if(i == 1){
+                arrayColumn[i] = new layout.ItemSpec(300, layout.GridUnitType.pixel);
+            } else {
+                    arrayColumn[i] = new layout.ItemSpec(1, layout.GridUnitType.star);
+                }
+                newGridLayout.addColumn(arrayColumn[i]);
+
+            }
+
+            // Create rows
+            for ( i = 0 ; i < 3 ; i++ )
+            {
+
+                arrayRow[i] = new layout.ItemSpec(1, layout.GridUnitType.star);
+                newGridLayout.addRow(arrayRow[i]);
+
+            }
+
+            for( i = 0 ; i < fieldsSize ; i++ ){
+
+                switch(localStorage.getItem("form_login_typeObject")[i])
+                {
+                
+                    case "label": fieldsArray[i] = new labelModule.Label();
+                    break;
+
+                    case "button": fieldsArray[i] = new buttonModule.Button();
+                                fieldsArray[i].on(buttonModule.Button.tapEvent , function(){
+                                    http.getString("https://luisfranciscocode.000webhostapp.com/fazerLogin.php?pin=" + page.getViewById(localStorage.getItem("form_login_idObject")[1]).text).then(function (r) {
+                                        if(r == "done"){
+                                        toast.makeText("Consegui camarada , yeeyye").show();
+                                    } else {
+                                        toast.makeText("Wasted").show();
+                                    }
+                                    }, function (e) {
+                                        alert(e);
+                                    });
+                                });
+                    break;
+
+                    case "textbox": fieldsArray[i] = new textFieldModule.TextField();
+                    break;
+
+                    case "image":  fieldsArray[i] = new imageModule.Image();
+                                fieldsArray[i].src = "~/views/login-view/Imagens/estagio.png";
+                    break;
+
+                    default:
+                    break;
+
+                }
+
+                // Add all properties to the fields , propriedades em comum
+
+                fieldsArray[i].text = localStorage.getItem("form_login_textObject")[i];
+                fieldsArray[i].id = localStorage.getItem("form_login_idObject")[i];
+                fieldsArray[i].value = localStorage.getItem("form_login_valueObject")[i]
+                fieldsArray[i].list = localStorage.getItem("form_login_listObject")[i];
+                // fieldsArray[i].height = myJSON[i].Fields.height;
+                // fieldsArray[i].width = myJSON[i].Fields.width;
+
+
+                // GridLayout stuff ( add , define location )
+                layout.GridLayout.setColumn(fieldsArray[i], parseInt(localStorage.getItem("form_login_colunaObject")[i]) );
+                layout.GridLayout.setRow(fieldsArray[i], parseInt(localStorage.getItem("form_login_linhaObject")[i]) );
+                newGridLayout.addChild(fieldsArray[i]);
+            }
+
+            i++;
+            
+            // Propriedades da layout
+            newGridLayout.backgroundImage = localStorage.getItem("form_login_backgroundImage");
+            newGridLayout.style.backgroundSize = localStorage.getItem("form_login_backgroundSize");
+            newGridLayout.style.backgroundRepeat = localStorage.getItem("form_login_backgroundRepeat");
+            page.content = newGridLayout;
+
+        }
+
+    else {
+
+        console.log("Foi buscar a newest versione");
+
+    // Get JSON
     http.getString("https://luisfranciscocode.000webhostapp.com/webservice.php?format=json&&form=form_login")
     .then(function (r) {
 
-        page = args.object;
+        var fieldsArray = Array();
+        var form_login_typeObject = Array();
+        var form_login_idObject = Array();
+        var form_login_textObject = Array();
+        var form_login_valueObject = Array();
+        var form_login_listObject = Array();
+        var form_login_colunaObject = Array();
+        var form_login_linhaObject = Array();
         
         var newGridLayout = new layout.GridLayout();
 
@@ -39,6 +140,9 @@ exports.load = function(args) {
 
         var arrayColumn = Array(fieldsSize);
         var arrayRow = Array(fieldsSize);
+
+        // Create Columns ( a primeira e a ultima sao autosize ,
+        // a do meio ( "1" ) é a que tem um tamanho especifico "pixel" )
 
         for ( i = 0 ; i < 3 ; i++ )
         {
@@ -51,6 +155,7 @@ exports.load = function(args) {
 
         }
 
+        // Create rows
          for ( i = 0 ; i < 3 ; i++ )
         {
 
@@ -59,51 +164,39 @@ exports.load = function(args) {
 
         }
 
-        for ( i = 0 ; i < fieldsSize ; i++ )
+        // Leitura dos Fields do JSON
+        for ( i = 0 ; i < fieldsSize - 1 ; i++ )
         {
             console.log(myJSON[i].Fields.type);
+
+            // Verificar tipo do field para defenir properties especificas e criar o tipo
+            // de field ( textbox , button ... ) , pois estam todos no array fieldsArray
             switch(myJSON[i].Fields.type)
             {
                 
-                case "label": labelArray[labelNumber] = new labelModule.Label();
-                              labelArray[labelNumber].id = myJSON[i].Fields.id;
-                              labelArray[labelNumber].text = myJSON[i].Fields.text;
-                              layout.GridLayout.setColumn(labelArray[labelNumber], parseInt(myJSON[i].Fields.column_grid));
-                              layout.GridLayout.setRow(labelArray[labelNumber], parseInt(myJSON[i].Fields.row_grid));
-                              newGridLayout.addChild(labelArray[labelNumber]);
-                              labelNumber++;
-                              
+                case "label": fieldsArray[i] = new labelModule.Label();
                 break;
 
-                case "button": buttonArray[buttonNumber] = new buttonModule.Button();
-                               buttonArray[buttonNumber].id = myJSON[i].Fields.id;
-                               buttonArray[buttonNumber].text = myJSON[i].Fields.text;
-                               buttonArray[buttonNumber].class = "btn";
-                               layout.GridLayout.setColumn(buttonArray[buttonNumber], parseInt(myJSON[i].Fields.column_grid));
-                               layout.GridLayout.setRow(buttonArray[buttonNumber], parseInt(myJSON[i].Fields.row_grid));
-                               newGridLayout.addChild(buttonArray[buttonNumber]);
-                               buttonNumber++;
+                case "button": fieldsArray[i] = new buttonModule.Button();
+                               fieldsArray[i].on(buttonModule.Button.tapEvent , function(){
+                                http.getString("https://luisfranciscocode.000webhostapp.com/fazerLogin.php?pin=" + page.getViewById(localStorage.getItem("form_login_idObject")[1]).text).then(function (r) {
+                                    if(r == "done"){
+                                        toast.makeText("Consegui camarada , yeeyye").show();
+                                    } else {
+                                        toast.makeText("Wasted").show();
+                                    }
+
+                                }, function (e) {
+                                    alert(e);
+                                });
+                               });
                 break;
 
-                case "textbox": textFieldArray[textFieldNumber] = new textFieldModule.TextField();
-                                textFieldArray[textFieldNumber].id = myJSON[i].Fields.id;
-                                textFieldArray[textFieldNumber].text = myJSON[i].Fields.text;
-                                layout.GridLayout.setColumn(textFieldArray[textFieldNumber], parseInt(myJSON[i].Fields.column_grid));
-                                layout.GridLayout.setRow(textFieldArray[textFieldNumber], parseInt(myJSON[i].Fields.row_grid));
-                                newGridLayout.addChild(textFieldArray[textFieldNumber]);
-                                textFieldNumber++;
-                              
+                case "textbox": fieldsArray[i] = new textFieldModule.TextField();
                 break;
 
-                case "image":  imageArray[imageNumber] = new imageModule.Image();
-                               imageArray[imageNumber].id = myJSON[i].Fields.id;
-                               imageArray[imageNumber].text = myJSON[i].Fields.text;
-                               imageArray[imageNumber].src = "~/views/login-view/Imagens/estagio.png";
-                               layout.GridLayout.setColumn(imageArray[imageNumber], parseInt(myJSON[i].Fields.column_grid));
-                               layout.GridLayout.setRow(imageArray[imageNumber], parseInt(myJSON[i].Fields.row_grid));
-                               newGridLayout.addChild(imageArray[imageNumber]);
-                               imageNumber++;
-                              
+                case "image":  fieldsArray[i] = new imageModule.Image();
+                               fieldsArray[i].src = "~/views/login-view/Imagens/estagio.png";
                 break;
 
                 default:
@@ -111,15 +204,58 @@ exports.load = function(args) {
 
             }
 
+            // Add all properties to the fields , propriedades em comum
+
+            fieldsArray[i].text = myJSON[i].Fields.text;
+            fieldsArray[i].id = myJSON[i].Fields.id;
+            fieldsArray[i].value = myJSON[i].Fields.value;
+            fieldsArray[i].list = myJSON[i].Fields.list;
+            // fieldsArray[i].height = myJSON[i].Fields.height;
+            // fieldsArray[i].width = myJSON[i].Fields.width;
+
+
+            // LocalStorage stuff ( add all fields to localStorage )
+
+            form_login_typeObject[i] = myJSON[i].Fields.type;
+            form_login_textObject[i] = myJSON[i].Fields.text;
+            form_login_idObject[i] = myJSON[i].Fields.id;
+            form_login_valueObject[i] = myJSON[i].Fields.value;
+            form_login_listObject[i] = myJSON[i].Fields.list;
+            form_login_colunaObject[i] = myJSON[i].Fields.col;
+            form_login_linhaObject[i] = myJSON[i].Fields.ro;
+
+            // GridLayout stuff ( add , define location )
+            layout.GridLayout.setColumn(fieldsArray[i], parseInt(myJSON[i].Fields.col ) );
+            layout.GridLayout.setRow(fieldsArray[i], parseInt(myJSON[i].Fields.ro) );
+            newGridLayout.addChild(fieldsArray[i]);
+            var ok = myJSON[i+1].Fields.id;
         }
-      
-        newGridLayout.backgroundImage = "~/views/login-view/Imagens/bg2.jpg";
-        newGridLayout.style.backgroundSize = "cover";
-        newGridLayout.style.backgroundRepeat= "no-repeat";
+        
+        // Propriedades da layout
+        var bgSource = "~/views/login-view/Imagens/bg2.jpg";
+        var bgSize = "cover";
+        var bgRepeat ="no-repeat";
+
+        newGridLayout.backgroundImage = bgSource;
+        newGridLayout.style.backgroundSize = bgSize;
+        newGridLayout.style.backgroundRepeat = bgRepeat;
         page.content = newGridLayout;
-        localStorage.setItem( "layout" , newGridLayout );
-        var k = "WTF" ;
-        localStorage.setItem( "appVersion" , k );
+
+        // Save info in localStorage
+
+        //var newVersion = myJSON[i].Fields.id;
+
+        localStorage.setItem( "form_login_backgroundImage" , bgSource );
+        localStorage.setItem( "form_login_backgroundSize" , bgSize );
+        localStorage.setItem( "form_login_backgroundRepeat" , bgRepeat );
+        localStorage.setItem( "form_login_typeObject" , form_login_typeObject );
+        localStorage.setItem( "form_login_textObject" , form_login_textObject );
+        localStorage.setItem( "form_login_idObject" , form_login_idObject );
+        localStorage.setItem( "form_login_valueObject" , form_login_valueObject );
+        localStorage.setItem( "form_login_listObject" , form_login_listObject );
+        localStorage.setItem( "form_login_colunaObject" , form_login_colunaObject );
+        localStorage.setItem( "form_login_linhaObject" , form_login_linhaObject );
+        localStorage.setItem( "form_login_appVersion" , ok );
 
 }, function (e) {
     alert(e);
@@ -128,19 +264,11 @@ exports.load = function(args) {
 
     }
 
-    else {
+    }, function (e) {
+        
+        alert(e);
 
-        fetch(localStorage.getItem("layout"))
-        .then( function(r){
-
-            page = args.object;
-
-            page.content = JSON.parse(r) ;
-
-        });
-
-    }
-
+    });
 
 
 }
