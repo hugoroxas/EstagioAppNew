b032bea15ed857d4cec00228ad68d9e85c1074a6
sendysestagio <sendyspedro@gmail.com>
2017-04-07 14:04:50 +0100

    FIX sumarios

--- NewApp/app/views/sumarios/criarnovo/criarnovo.js
+++ NewApp/app/views/sumarios/criarnovo/criarnovo.js
@@ -74,23 +74,45 @@ http.getJSON("https://luisfranciscocode.000webhostapp.com/webservice.php?format=
                 console.log("MEU TYPE: "+ jsonfile[i].Fields.type);
                 if(jsonfile[i].Fields.type == "TextField"){
                     txt1 = new textViewModule.TextView();
-                    localStorage.setItem("elemento2ID_type"+i, "textview");
-                    localStorage.setItem("elemento2ID_id"+i,"txt_sumario");
+                    localStorage.setItem("elemento2ID_type"+i, jsonfile[i].Fields.type);
+                    localStorage.setItem("elemento2ID_id"+i,jsonfile[i].Fields.id);
+                    txt1.id = jsonfile[i].Fields.id;
                     //txt1.hint = "Sumário";
                     stackLayout.addChild(txt1);
 
                 }else if(jsonfile[i].Fields.type == "Button"){
                     var btn2 = new buttonModule.Button();
-                    localStorage.setItem("elemento2ID_type"+i, "button");
+                    localStorage.setItem("elemento2ID_type"+i, jsonfile[i].Fields.type);
                     var btnid = jsonfile[i].Fields.id;
                     localStorage.setItem("elemento2ID_id"+i, btnid);
+                    btn2.id = jsonfile[i].Fields.id;
                     //var txt_tamanho = txt1.text;
                     //console.log("TATAMNHO123: " + txt_tamanho);
                     
                     if(btnid == "confirma_add"){
                     btn2.on(btnModule.Button.tapEvent, function(){
                         if(txt1.text != ""){
-                            alert("Adiciona");
+                        page.getViewById("txt_sumario").dismissSoftInput();
+                        datahoje_antes = new Date();
+                        meu_year = datahoje_antes.getFullYear();
+                        meu_month = datahoje_antes.getMonth()+1;
+                        meu_day = datahoje_antes.getDate();
+                        datacompleta = meu_year+"-"+meu_month+"-"+meu_day;
+                        datahoje = datacompleta;
+                        page.getViewById("lbl_data").text = datahoje;
+                        
+
+                        var txtfinal_antes = page.getViewById("txt_sumario").text;
+                        var txtfinal = txtfinal_antes.replace(/ /g,"_");
+                        var datafinal = datahoje;
+
+                        console.log("txtfinal: " + txtfinal);
+                        console.log("datafinal: " + datafinal);
+
+                        http.getString("https://luisfranciscocode.000webhostapp.com/adicionar_sumario.php?lbl_data="+datafinal+"&&txt_sumario="+txtfinal).then(function (r){
+                            console.log(r);
+                        });
+                        topmost.navigate("views/sumarios/sumarios");
                         }else{
                             alert("Escreva texto no sumário.");
                         }
@@ -124,8 +146,9 @@ http.getJSON("https://luisfranciscocode.000webhostapp.com/webservice.php?format=
                     //stackLayout.addChild(btn1);
                 }else if(jsonfile[i].Fields.type == "Label"){
                     var lbl_data = new labelModule.Label();
-                    localStorage.setItem("elemento2ID_type"+i, "label");
+                    localStorage.setItem("elemento2ID_type"+i, jsonfile[i].Fields.type);
                     var lbl_id = jsonfile[i].Fields.id;
+                    lbl_data.id = jsonfile[i].Fields.id;
                     localStorage.setItem("elemento2ID_id"+i, lbl_id);
                     
                     
@@ -152,14 +175,14 @@ http.getJSON("https://luisfranciscocode.000webhostapp.com/webservice.php?format=
                 for(i=0;i<100;i++){
                 var curr_obj = localStorage.getItem("elemento2ID_type"+i);
                 console.log("ELEMENTOID: " + curr_obj);
-                if(curr_obj == "button"){
+                if(curr_obj == "Button"){
                     if(localStorage.getItem("elemento2ID_id"+i) == "confirma_add"){
                     var btn1 = new buttonModule.Button();
                     btn1.text = localStorage.getItem("elemento2ID_text"+i);
                     //btn1.width = localStorage.getItem("elemento2ID_width"+i);
                     //btn1.height = localStorage.getItem("elemento2ID_height"+i);
                     btn1.on(btnModule.Button.tapEvent, function(){
-
+                        if(txt1.text != ""){
                         page.getViewById("txt_sumario").dismissSoftInput();
                         datahoje_antes = new Date();
                         meu_year = datahoje_antes.getFullYear();
@@ -181,6 +204,9 @@ http.getJSON("https://luisfranciscocode.000webhostapp.com/webservice.php?format=
                             console.log(r);
                         });
                         topmost.navigate("views/sumarios/sumarios");
+                        }else{
+                            alert("Escreva texto no sumário.");
+                        }
                     });
                     stackLayout.addChild(btn1);
                 }else if(localStorage.getItem("elemento2ID_id"+i) == "btn_voltar_menu"){
@@ -194,16 +220,17 @@ http.getJSON("https://luisfranciscocode.000webhostapp.com/webservice.php?format=
                     stackLayout.addChild(btn1);
                 }
 
-                }else if(curr_obj == "listview"){
+                }else if(curr_obj == "ListView"){
                     var lista1 = new listViewModule.ListView();
                     lista1.items = localStorage.getItem("elemento2ID_items"+i);
                     stackLayout.addChild(lista1);
-                }else if(curr_obj == "textview"){
+                }else if(curr_obj == "TextField"){
+                    //alert("TEXT");
                     var txt1 = new textViewModule.TextView();
                     txt1.id = localStorage.getItem("elemento2ID_id"+i);
                     //txt1.items = localStorage.getItem("elemento2ID_items"+i);
                     stackLayout.addChild(txt1);
-                }else if(curr_obj == "label"){
+                }else if(curr_obj == "Label"){
                     if(localStorage.getItem("elemento2ID_id"+i) == "lbl_data"){
                     var lbl1 = new labelModule.Label();
                     lbl1.id = localStorage.getItem("elemento2ID_id"+i);
