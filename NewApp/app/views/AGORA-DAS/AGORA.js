b4768d9e710f6e2b2ce5780eef8ba4feab34fdb6
lbernasl <miguelbernardinoferreira@gmail.com>
2017-04-07 17:56:28 +0100

    YEAH BOIII

--- NewApp/app/views/AGORA-DAS/AGORA.js
+++ NewApp/app/views/AGORA-DAS/AGORA.js
@@ -5,37 +5,33 @@ var page;
 var http = require("http");
 
 exports.listLoad = function(args){
-
+    page = args.object;
+    
     http.getJSON("https://luisfranciscocode.000webhostapp.com/webservice.php?format=json&&form=form_lista_tarefas")
     .then(function (r) {
-        page = args.object;
         var newStackLayout = new layout.StackLayout();
         var tarefasList = new listViewModule.ListView();
         var buttoooon = new buttonModule.Button();
-
+        var array_dados = new Array();
         var myJason = r;
         var fieldsSize = myJason.length;
 
         for(i = 0; i < fieldsSize; i++){
             switch (myJason[i].Fields.type){
                 case "ListView":
-                    http.getString("https://luisfranciscocode.000webhostapp.com/numDados.php")
-                    .then(function (r){
-                        var array_dados = [r];
-                        for(k=1; k <= r; k++) {
-                            http.getstring("https://luisfranciscocode.000webhostapp.com/dadosList.php?id=" + k)
-                            .then(function (r){
-                                array_dados[k] = r;
-                            }, function(e) {
-                                console.log(e);
-                            })
-                        }
+                    http.getString("https://luisfranciscocode.000webhostapp.com/dadosList.php")
+                    .then(function (q){
+                        
+                        var daaaados = q.replace(/"/g, '')
+                        array_dados = daaaados.split(',');
+                        tarefasList.items = "";
                         tarefasList.items = array_dados;
                         newStackLayout.addChild(tarefasList);
                     }, function(e) {
                         console.log(e);
                     });
-                    
+                    tarefasList.items = "";
+                    tarefasList.items = array_dados;
                     newStackLayout.addChild(tarefasList);
                     break;
                 case "button":
