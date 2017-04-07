a332a9fe31ce50c7ddbf5e9e1485d947ec5518e7
LuisFranciscoCode <luisfranciscocode@gmail.com>
2017-04-07 09:50:32 +0100

    tentar

--- NewApp/platforms/android/build/intermediates/assets/F0F1/debug/app/app.js
+++ NewApp/platforms/android/build/intermediates/assets/F0F1/debug/app/app.js
@@ -1,12 +1,4 @@
 var applicationModule = require("application");
-<<<<<<< HEAD
-applicationModule.start({ moduleName: "views/menu-view/menu" });
-=======
-<<<<<<< HEAD
-applicationModule.start({ moduleName: "views/menu-view/menu" });
-=======
-applicationModule.start({ moduleName: "views/login-view/login" });
->>>>>>> origin/master
 
+applicationModule.start({ moduleName: "views/login-view/login" });
 var page;
->>>>>>> b7215b69aec7e11fa67e132e9eb14e1ab52dc7cd
